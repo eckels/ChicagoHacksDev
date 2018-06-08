303efd8ef1ba9cff6bf187b1646f30864ff4308c
Mingjie Jiang <jiang@mingjie.info>
2018-06-07 23:58:58 -0400

    Minor errors

--- js/hackchicago.js
+++ js/hackchicago.js
@@ -9,10 +9,10 @@ var hc = new tingle.modal({
 });
 
 // set content
-hc.setContent('<div style="text-align: center"><img src="https://hackchicago.io/img/bluelogo.svg" style="width: 200px"><h1>Check out <strong>Hack Chicago</strong>!</h1><p>We are striving to improve the event by making it bigger and better. Through a partnership with Hack Club, we will be bringing Hack Chicago, an even larger event to the Midwest. With more attendees, activities, prizes, and hackers, we expect this year to be a blast!</p></div>');
+hc.setContent('<div style="text-align: center"><img src="https://hackchicago.io/img/bluelogo.svg" style="width: 200px"><h1>Check out <strong>Hack Chicago</strong>!</h1><p>We are striving to improve the event by making it bigger and better. Through a partnership with Hack Club, we will be bringing Hack Chicago, an even larger event, to the Midwest. With more attendees, activities, prizes, and hackers, we expect this year to be a blast!</p></div>');
 
 // add a button
-hc.addFooterBtn('Visit', 'tingle-btn tingle-btn--primary', function() { window.location.href = "https://hackchicago.io"});
+hc.addFooterBtn('Join us now!', 'tingle-btn tingle-btn--primary', function() { window.location.href = "https://hackchicago.io"});
 
 window.onload = function() {
     hc.open();
